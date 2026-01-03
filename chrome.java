b1c09c5e2e09559ec8963de90e9d2a3cbd9792db
saranyatamil13 <ksaranya.93@gmail.com>
2026-01-03 21:57:28 +0530

    Add files via upload
    chrome assignment

---
+++ chrome.java
@@ -0,0 +1,17 @@
+package assignments;
+
+public class chrome {
+	public static void main(String args[]) {
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear=2008;
+		char shortcutKey = 'C';
+		System.out.println(version);
+		System.out.println(developer);
+		System.out.println(isBeta);
+		System.out.println(releaseYear);
+		System.out.println(shortcutKey);
+	}
+
+}
